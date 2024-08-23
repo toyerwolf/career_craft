796af424f173a9a554931ee5fbb3ead1f00ff0c7
Huseyn Mamedov <huseynmamedov472@gmail.com>
2024-08-23 12:01:06 +0400

    12

--- src/test/java/com/example/careercraft/CareercraftApplicationTests.java
+++ src/test/java/com/example/careercraft/CareercraftApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.careercraft;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CareercraftApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.careercraft;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CareercraftApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
